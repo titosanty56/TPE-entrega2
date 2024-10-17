3ecc6e0b26e2c7e1fd47f91d5f9d2a6c7f55e9b8
SimonMarciante <simonmarciante5@gmail.com>
2024-10-17 20:13:11 -0300

    Delete aerolineas.php

--- aerolineas.php
+++
@@ -1,6 +0,0 @@
-
-<?php
-echo "<h1><a href='aerolineasargentina.php'>Aerolineas Argentina</a></h1>";
-echo "<h1><a href='skyairline.php'>Sky Airline</a></h1>";
-echo "<h1><a href='qatararways.php'>Qatar Airways</a></h1>";
-?>
