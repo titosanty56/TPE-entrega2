e39dfab820dd6831560389b43b472c2031fabdc1
santi <=santiagovepa@hotmail.com>
2024-10-17 20:44:59 -0300

    actualizo cambios

--- router.php
+++ router.php
@@ -2,6 +2,11 @@
 
 require_once './app/controllers/aerolineas.controllers.php';
 require_once './app/controllers/personas.controllers.php';
+//require_once 'libs/response.php';
+//require_once 'app/middlewares/session.auth.middleware.php';
+//require_once 'app/middlewares/verify.auth.middleware.php';
+require_once 'app/controllers/auth.controllers.php';
+
 
 define('BASE_URL', '//'.$_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']).'/');
 
@@ -29,40 +34,6 @@ switch($params[0]){
         $controller = new modelosControllers();
         $controller -> showModeloDetails($params[1]);
             break;
-}
-// parsea la accion para separar accion real de parametros
-$params = explode('/', $action);
-
-//require_once 'libs/response.php';
-//require_once 'app/middlewares/session.auth.middleware.php';
-//require_once 'app/middlewares/verify.auth.middleware.php';
-require_once 'app/controllers/task.controllers.php';
-require_once 'app/controllers/auth.controllers.php';
-
-// base_url para redirecciones y base tag
-define('BASE_URL', '//'.$_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']).'/');
-
-$res = new Response();
-
-$action = 'listar'; // accion por defecto si no se envia ninguna
-if (!empty( $_GET['action'])) {
-    $action = $_GET['action'];
-}
-
-// tabla de ruteo
-
-// listar  -> TaskController->showTask();
-// nueva  -> TaskController->addTask();
-// eliminar/:ID  -> TaskController->deleteTask($id);
-// finalizar/:ID -> TaskController->finishTask($id);
-// ver/:ID -> TaskController->view($id); COMPLETAR
-
-switch ($params[0]) {
-    case 'listar':
-        sessionAuthMiddleware($res);
-        $controller = new TaskController($res);
-        $controller->showTasks();
-        break;
     case 'nueva':
         sessionAuthMiddleware($res); // Setea $res->user si existe session
         verifyAuthMiddleware($res); // Verifica que el usuario esté logueado o redirige a login
